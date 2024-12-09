4efa9b8c4c07ca56510e84cb83aea322482c0b1d
Ajith B <2299419@tcs.com>
2024-12-09 14:39:59 +0530

    calculator

--- sripts.js
+++ sripts.js
@@ -1,5 +1,18 @@
 // scripts.js
 
-function showAlert() {
-    alert("Button clicked!");
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
 }
